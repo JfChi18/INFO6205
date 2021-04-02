e421993e61b8603a7eb80e60e4b826451d95f8d6
choco.lette <gretchenchi@gmail.com>
2021-04-01 20:24:36 -0700

    Update and rename Q3 to Q3.java

---
+++ Midterm/Q3.java
@@ -0,0 +1,25 @@
+class Solution {
+    //Time Complexity: O(N), Space Complexity: O(1)
+
+    public int getMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] == 1) {
+                count ++;
+            }
+            else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        max = Math.max(max, count);
+        return max;
+    }
+}
+public static void main(String[] args)
+    {
+        int nums[] = {0,1,0,1,1,0,1,1,1,0,0,0};
+        getMaxConsecutiveOnes(max);
+    }
+}
