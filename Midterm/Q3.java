2bb88aeb27690ef4fa73659d123a1caa2a4e60fb
choco.lette <gretchenchi@gmail.com>
2021-04-01 20:32:36 -0700

    Update Q3.java

--- Midterm/Q3.java
+++ Midterm/Q3.java
@@ -16,7 +16,7 @@ class Solution {
         max = Math.max(max, count);
         return max;
     }
-}
+
 public static void main(String[] args)
     {
         int nums[] = {0,1,0,1,1,0,1,1,1,0,0,0};
